c56a7afcf595b96370e144d6b73f5bec52226979
Sasha_PowerPoint <vladnovod7@gmail.com>
2019-01-10 22:01:36 +0200

    Перевірка фетча

--- js/script.js
+++ js/script.js
@@ -1,50 +1,69 @@
-var ctx = setupCanvas(document.querySelector("canvas"));
+/*var ctx = setupCanvas(document.querySelector("canvas"));
 var ctx = canvas.getContext("2d");
 ctx.fillStyle = "#000000";
 ctx.fillRect($(window).width() / 4, $(window).height() / 4, 2, 2);
-ctx.lineWidth = 3;
+ctx.lineWidth = 3;*/
 
-var dano = {};
-var rozrah = {};
+var data = {};
 var base = {};
 var koef =350;
+var db_got =[
+    {
+        name: "Антонов",
+        data: "V=xt"
+    },
+    {
+        name: "МоторСіч",
+        data: "ппопооп"
+    },
+    {
+        name: "Луч йобаний",
+        data: "кардан"
+    },
+];
 
+function ServeBlocksFromDatabase(){
+    fetch("test.txt")
+        .then(function(res){
+            return res.text();
+        })
+        .then(function(res){
+            console.log(res);
+        });
+}
 function Calculate() {
-    dano.V_e = 2;
-    dano.Roh = 1.24 * Math.pow(1 - (dano.H / 44300), 4.256);
-    dano.Ro = dano.Roh * (1 + 0.5 * Math.pow(dano.Vh / 310, 2) * (1 - Math.pow(dano.V_e, 2)));
-    dano.k2 = 1.2;
-    dano.k4 = 0.8;
-    rozrah.L = dano.Lop + dano.Lukl - dano.DeltaL;
-    rozrah.Fe = dano.Gv / (dano.V_e * dano.Vh * dano.Ro);
-    rozrah.Fe_t = rozrah.Fe + Math.PI * Math.pow(Yop1(rozrah.L),2);
-    rozrah.De = Math.sqrt((4 * rozrah.Fe_t) / Math.PI);
-    rozrah.Dm = dano.k2 * rozrah.De;
-    rozrah.Delta = (rozrah.Dm - rozrah.De) / 2;
-    rozrah.Ro_min = Math.pow(rozrah.Delta, 2) / rozrah.L;
-    rozrah.Fmin = dano.k4 * rozrah.Fe;
-    rozrah.Lop_Lop1 = dano.Lop / Math.sqrt(1 - Math.pow(dano.dv - dano.Dv, 2));
-    rozrah.c = (
-                Math.PI * Math.pow(dano.Dv, 2) *
+    data.V_e = 2;
+    data.Roh = 1.24 * Math.pow(1 - (data.H / 44300), 4.256);
+    data.Ro = data.Roh * (1 + 0.5 * Math.pow(data.Vh / 310, 2) * (1 - Math.pow(data.V_e, 2)));
+    data.k2 = 1.2;
+    data.k4 = 0.8;
+    data.L = data.Lop + data.Lukl - data.DeltaL;
+    data.Fe = data.Gv / (data.V_e * data.Vh * data.Ro);
+    data.Fe_t = data.Fe + Math.PI * Math.pow(Yop1(data.L),2);
+    data.De = Math.sqrt((4 * data.Fe_t) / Math.PI);
+    data.Dm = data.k2 * data.De;
+    data.Delta = (data.Dm - data.De) / 2;
+    data.Ro_min = Math.pow(data.Delta, 2) / data.L;
+    data.Fmin = data.k4 * data.Fe;
+    data.Lop_Lop1 = data.Lop / Math.sqrt(1 - Math.pow(data.dv - data.Dv, 2));
+    data.c = (
+                Math.PI * Math.pow(data.Dv, 2) *
                     (
-                        Math.pow(2 * dano.DeltaL + rozrah.De, 2) - 4 * Math.pow(rozrah.Lop_Lop1, 2)
+                        Math.pow(2 * data.DeltaL + data.De, 2) - 4 * Math.pow(data.Lop_Lop1, 2)
                     )
-                    - 16 * Math.pow(rozrah.Lop_Lop1, 2) * rozrah.Fmin
+                    - 16 * Math.pow(data.Lop_Lop1, 2) * data.Fmin
                 )
                 / Math.PI *
                     (
-                        4 * Math.pow(rozrah.Lop_Lop1, 2) + Math.pow(dano.Dv, 2)
+                        4 * Math.pow(data.Lop_Lop1, 2) + Math.pow(data.Dv, 2)
                     );
-    rozrah.k = -1 * ((Math.pow(dano.Dv, 2) * (2 * dano.DeltaL + rozrah.De)) / (4 * (Math.pow(rozrah.Lop_Lop1, 2)) + Math.pow(dano.Dv, 2)));
-    rozrah.Dmin = -1 * rozrah.k + Math.sqrt(Math.pow(rozrah.k, 2) - rozrah.c);
-    rozrah.R = (rozrah.De - rozrah.Dmin) / 8;
-    rozrah.TgBx = (Math.pow(dano.Ddv, 2) - Math.pow(dano.dv, 2)) / (2 * dano.Ddv * dano.Lop);
-    rozrah.F2 = (Math.PI / 4) * (Math.pow(dano.Ddv, 2) - 4 * Math.pow(Yop2(0), 2));
+    data.k = -1 * ((Math.pow(data.Dv, 2) * (2 * data.DeltaL + data.De)) / (4 * (Math.pow(data.Lop_Lop1, 2)) + Math.pow(data.Dv, 2)));
+    data.Dmin = -1 * data.k + Math.sqrt(Math.pow(data.k, 2) - data.c);
+    data.R = (data.De - data.Dmin) / 8;
+    data.TgBx = (Math.pow(data.Ddv, 2) - Math.pow(data.dv, 2)) / (2 * data.Ddv * data.Lop);
+    data.F2 = (Math.PI / 4) * (Math.pow(data.Ddv, 2) - 4 * Math.pow(Yop2(0), 2));
     console.log(base);
 
-    $(".canvas_hover").addClass("canvas_hover_active");
-    $(".shader").addClass("shader_active");
-   // AddItemToFolder();
    /* Yv();
     Yv2();
     Yop();
@@ -55,76 +74,7 @@ function Calculate() {
     DrawDesc();*/
 };
 
-function Calculate2(j) {
-    var dano = base[j].dano;
-    var rozrah = base[j].rozrah;
-    dano.V_e = 2;
-    dano.Roh = 1.24 * Math.pow(1 - (dano.H / 44300), 4.256);
-    dano.Ro = dano.Roh * (1 + 0.5 * Math.pow(dano.Vh / 310, 2) * (1 - Math.pow(dano.V_e, 2)));
-    dano.k2 = 1.2;
-    dano.k4 = 0.8;
-    /////////////////////////////////////////////////
-    rozrah.L = dano.Lop + dano.Lukl - dano.DeltaL;
-    rozrah.Fe = dano.Gv / (dano.V_e * dano.Vh * dano.Ro);
-    rozrah.Fe_t = rozrah.Fe + Math.PI * Math.pow(Yop1(rozrah.L),2);
-    rozrah.De = Math.sqrt((4 * rozrah.Fe_t) / Math.PI);
-    rozrah.Dm = dano.k2 * rozrah.De;
-    rozrah.Delta = (rozrah.Dm - rozrah.De) / 2;
-    rozrah.Ro_min = Math.pow(rozrah.Delta, 2) / rozrah.L;
-    rozrah.Fmin = dano.k4 * rozrah.Fe;
-    rozrah.Lop_Lop1 = dano.Lop / Math.sqrt(1 - Math.pow(dano.dv - dano.Dv, 2));
-    rozrah.c = (
-            Math.PI * Math.pow(dano.Dv, 2) *
-            (
-                Math.pow(2 * dano.DeltaL + rozrah.De, 2) - 4 * Math.pow(rozrah.Lop_Lop1, 2)
-            )
-            - 16 * Math.pow(rozrah.Lop_Lop1, 2) * rozrah.Fmin
-        )
-        / Math.PI *
-        (
-            4 * Math.pow(rozrah.Lop_Lop1, 2) + Math.pow(dano.Dv, 2)
-        );
-    rozrah.k = -1 * ((Math.pow(dano.Dv, 2) * (2 * dano.DeltaL + rozrah.De)) / (4 * (Math.pow(rozrah.Lop_Lop1, 2)) + Math.pow(dano.Dv, 2)));
-    rozrah.Dmin = -1 * rozrah.k + Math.sqrt(Math.pow(rozrah.k, 2) - rozrah.c);
-    rozrah.R = (rozrah.De - rozrah.Dmin) / 8;
-    rozrah.TgBx = (Math.pow(dano.Ddv, 2) - Math.pow(dano.dv, 2)) / (2 * dano.Ddv * dano.Lop);
-    rozrah.F2 = (Math.PI / 4) * (Math.pow(dano.Ddv, 2) - 4 * Math.pow(Yop2(0), 2));
-    //   console.log(dano);
-    //   console.log(rozrah);
 
-
-    console.log(base);
-
-    $(".canvas_hover").addClass("canvas_hover_active");
-    $(".shader").addClass("shader_active");
-    // AddItemToFolder();
-    Yv();
-    Yv2();
-    Yop();
-    Yvnesh();
-    Yvi1();
-    Yop2_draw();
-    Radius();
-    DrawDesc();
-};
-
-$(".red_cross").click(function(){
-    $(".canvas_hover").removeClass("canvas_hover_active");
-    $(".shader").removeClass("shader_active");
-});
-function CreateItem(){
-    var item = document.createElement("div");
-    $(item).addClass("folder_item");
-    $(item).html(
-        "<p>" + dano.name + "</p>"
-    );
-    item.addEventListener("click", function(){
-        alert($(item).children().html());
-        Calculate2($(item).children().html());
-    });
-    var list = $(".folder");
-    $(".folder").append(item);
-}
 function GetBase(){
     var xhr = new XMLHttpRequest();
     xhr.open("POST", "getBase.php", true);
@@ -138,10 +88,10 @@ function GetBase(){
 }
 function SendToBase(){
     var dani = {};
-    dani.dano = dano;
-    dani.rozrah = rozrah;
-   // alert(dano.name);
-    base[dano.name] = dani;
+    dani.data = data;
+    dani.data = data;
+   // alert(data.name);
+    base[data.name] = dani;
     //   console.log(base);
     var xhr = new XMLHttpRequest();
     xhr.open("POST", "saveItem.php", true);
@@ -157,45 +107,45 @@ function SendToBase(){
 }
 function Refill(name){
     console.log("gat = " + name );
-    for(var i in base[name].dano){
-        dano[i] = base[name].dano[i];
+    for(var i in base[name].data){
+        data[i] = base[name].data[i];
         console.log("name = " + i);
-        console.log("dano[i] = " + dano[i]);
-        console.log("base[name].dano[i] = "+ base[name].dano[i]);
+        console.log("data[i] = " + data[i]);
+        console.log("base[name].data[i] = "+ base[name].data[i]);
     }
-    for(var j in base[name].rozrah){
-        rozrah[j] = base[name].rozrah[j];
+    for(var j in base[name].data){
+        data[j] = base[name].data[j];
     }
 }
 function DrawDesc(){
     var desc = $(".canvas_desc");
-    $("#canvas_name").html(dano.name);
+    $("#canvas_name").html(data.name);
     $(desc).html(
-        "<li>" + "L = "+ rozrah.L.toFixed(3) +"</li>" +
-        "<li>" + "De = "+ rozrah.De.toFixed(3) +"</li>" +
-        "<li>" + "Dm = "+ rozrah.Dm.toFixed(3) +"</li>" +
-        "<li>" + "Delta = "+ rozrah.Delta.toFixed(3) +"</li>" +
-        "<li>" + "Dmin = "+ rozrah.Dmin.toFixed(3) +"</li>" +
-        "<li>" + "R = "+ rozrah.R.toFixed(3) +"</li>"
+        "<li>" + "L = "+ data.L.toFixed(3) +"</li>" +
+        "<li>" + "De = "+ data.De.toFixed(3) +"</li>" +
+        "<li>" + "Dm = "+ data.Dm.toFixed(3) +"</li>" +
+        "<li>" + "Delta = "+ data.Delta.toFixed(3) +"</li>" +
+        "<li>" + "Dmin = "+ data.Dmin.toFixed(3) +"</li>" +
+        "<li>" + "R = "+ data.R.toFixed(3) +"</li>"
     );
 }
 
 function Yv() {
-    var high = dano.Lukl + dano.Lop + dano.Lv2 + dano.Lv;
-    var low = dano.Lukl + dano.Lop + dano.Lv2;
+    var high = data.Lukl + data.Lop + data.Lv2 + data.Lv;
+    var low = data.Lukl + data.Lop + data.Lv2;
     var prev = {};
     prev.x = high;
     prev.y1 = 0;
     prev.y2 = 0;
     for (var x = high; x >= low; x = x - 0.01) {
-        var Yv1 = (dano.Dv / 2) * Math.sqrt(1 - (Math.pow(x - (rozrah.L + dano.DeltaL + dano.Lv2), 2) / (Math.pow(dano.Lv, 2))));
+        var Yv1 = (data.Dv / 2) * Math.sqrt(1 - (Math.pow(x - (data.L + data.DeltaL + data.Lv2), 2) / (Math.pow(data.Lv, 2))));
         ctx.fillStyle = "#FFFFFF";
 
         ctx.beginPath();
         ctx.moveTo(center.width - prev.x * koef, center.height - prev.y1 * koef);
         ctx.lineTo(center.width - x * koef, center.height - Yv1 * koef);
         ctx.stroke();
-        var Yv2 = -1 * (dano.Dv / 2) * Math.sqrt(1 - (Math.pow(x - (rozrah.L + dano.DeltaL + dano.Lv2), 2) / (Math.pow(dano.Lv, 2))));
+        var Yv2 = -1 * (data.Dv / 2) * Math.sqrt(1 - (Math.pow(x - (data.L + data.DeltaL + data.Lv2), 2) / (Math.pow(data.Lv, 2))));
         ctx.beginPath();
         ctx.moveTo(center.width - prev.x * koef, center.height - prev.y2 * koef);
         ctx.lineTo(center.width - x * koef, center.height - Yv2 * koef);
@@ -210,10 +160,10 @@ function Yv() {
 }
 
 function Yv2() {
-    var high = dano.Lukl + dano.Lop + dano.Lv2 + 0.01;
-    var low = dano.Lukl + dano.Lop;
-    var y1 = dano.Dv / 2;
-    var y2 = -dano.Dv / 2;
+    var high = data.Lukl + data.Lop + data.Lv2 + 0.01;
+    var low = data.Lukl + data.Lop;
+    var y1 = data.Dv / 2;
+    var y2 = -data.Dv / 2;
     ctx.beginPath();
     ctx.moveTo(center.width - high * koef - 1, center.height - y1 * koef);
     ctx.lineTo(center.width - low * koef, center.height - y1 * koef);
@@ -223,20 +173,20 @@ function Yv2() {
 }
 
 function Yop() {
-    var high = dano.Lukl + dano.Lop;
-    var low = dano.Lukl;
+    var high = data.Lukl + data.Lop;
+    var low = data.Lukl;
     var prev = {};
     prev.x = high;
-    prev.y1 = dano.Dv / 2;
-    prev.y2 = -dano.Dv / 2;
+    prev.y1 = data.Dv / 2;
+    prev.y2 = -data.Dv / 2;
     for (var x = high; x >= low; x = x - 0.01) {
-        var Yv1 = 1 * (dano.Dv / 2) * Math.sqrt(1 - ((Math.pow(x - (rozrah.L + dano.DeltaL), 2)) / (Math.pow(dano.Lop + (dano.Lop / (Math.sqrt(1 - (Math.pow(dano.dv / dano.Dv, 2))))), 2))));
+        var Yv1 = 1 * (data.Dv / 2) * Math.sqrt(1 - ((Math.pow(x - (data.L + data.DeltaL), 2)) / (Math.pow(data.Lop + (data.Lop / (Math.sqrt(1 - (Math.pow(data.dv / data.Dv, 2))))), 2))));
         ctx.fillStyle = "#FFFFFF";
         ctx.beginPath();
         ctx.moveTo(center.width - prev.x * koef, center.height - prev.y1 * koef);
         ctx.lineTo(center.width - x * koef, center.height - Yv1 * koef);
         ctx.stroke();
-        var Yv2 = -1 * (dano.Dv / 2) * Math.sqrt(1 - ((Math.pow(x - (rozrah.L + dano.DeltaL), 2)) / (Math.pow(dano.Lop + (dano.Lop / (Math.sqrt(1 - (Math.pow(dano.dv / dano.Dv, 2))))), 2))));
+        var Yv2 = -1 * (data.Dv / 2) * Math.sqrt(1 - ((Math.pow(x - (data.L + data.DeltaL), 2)) / (Math.pow(data.Lop + (data.Lop / (Math.sqrt(1 - (Math.pow(data.dv / data.Dv, 2))))), 2))));
         ctx.beginPath();
         ctx.moveTo(center.width - prev.x * koef, center.height - prev.y2 * koef);
         ctx.lineTo(center.width - x * koef, center.height - Yv2 * koef);
@@ -249,19 +199,19 @@ function Yop() {
 }
 
 function Yvnesh() {
-    var high = rozrah.L;
+    var high = data.L;
     var low = 0;
     var prev = {};
     prev.x = high;
-    prev.y1 = rozrah.De / 2 + rozrah.Delta * Math.sqrt(1 - (Math.pow(high, 2) / (Math.pow(rozrah.L, 2))));
-    prev.y2 = -rozrah.De / 2 + rozrah.Delta * Math.sqrt(1 - (Math.pow(high, 2) / (Math.pow(rozrah.L, 2))));
+    prev.y1 = data.De / 2 + data.Delta * Math.sqrt(1 - (Math.pow(high, 2) / (Math.pow(data.L, 2))));
+    prev.y2 = -data.De / 2 + data.Delta * Math.sqrt(1 - (Math.pow(high, 2) / (Math.pow(data.L, 2))));
     for (var x = high; x >= low; x = x - 0.01) {
-        var Yv1 = (rozrah.De / 2 + rozrah.Delta * Math.sqrt(1 - (Math.pow(x, 2)) / (Math.pow(rozrah.L, 2))));
+        var Yv1 = (data.De / 2 + data.Delta * Math.sqrt(1 - (Math.pow(x, 2)) / (Math.pow(data.L, 2))));
         ctx.beginPath();
         ctx.moveTo(center.width - prev.x * koef, center.height - prev.y1 * koef);
         ctx.lineTo(center.width - x * koef, center.height - Yv1 * koef);
         ctx.stroke();
-        var Yv2 = -(rozrah.De / 2 + rozrah.Delta * Math.sqrt(1 - (Math.pow(x, 2) / (Math.pow(rozrah.L, 2)))));
+        var Yv2 = -(data.De / 2 + data.Delta * Math.sqrt(1 - (Math.pow(x, 2) / (Math.pow(data.L, 2)))));
         ctx.beginPath();
         ctx.moveTo(center.width - prev.x * koef, center.height - prev.y2 * koef);
         ctx.lineTo(center.width - x * koef, center.height - Yv2 * koef);
@@ -274,16 +224,16 @@ function Yvnesh() {
 }
 
 function Yop1(x) {
-    var n =  1 * (dano.Dv / 2) * Math.sqrt(1 - ((Math.pow(x - (rozrah.L + dano.DeltaL), 2)) / (Math.pow(dano.Lop + (dano.Lop / (Math.sqrt(1 - (Math.pow(dano.dv / dano.Dv, 2))))), 2))));
+    var n =  1 * (data.Dv / 2) * Math.sqrt(1 - ((Math.pow(x - (data.L + data.DeltaL), 2)) / (Math.pow(data.Lop + (data.Lop / (Math.sqrt(1 - (Math.pow(data.dv / data.Dv, 2))))), 2))));
     return n;
 }
 function Yop2(x) {
-    var n = (dano.Ddv / 2 + rozrah.TgBx * (x - dano.Lukl));
+    var n = (data.Ddv / 2 + data.TgBx * (x - data.Lukl));
     return n;
 }
 
 function Yop2_draw(){
-    var high = dano.Lukl;
+    var high = data.Lukl;
     var low = 0;
     var prev = {};
     prev.x = high;
@@ -307,15 +257,15 @@ function Yop2_draw(){
     ;
 }
 function Radius(){
-    var high = rozrah.L;
-    var low = rozrah.L - rozrah.R;
+    var high = data.L;
+    var low = data.L - data.R;
     var prev = {};
     prev.x = high;
-    prev.y1 = (rozrah.De/2) - Math.sqrt(Math.pow(rozrah.R,2) - Math.pow(high - rozrah.L + rozrah.R,2));
+    prev.y1 = (data.De/2) - Math.sqrt(Math.pow(data.R,2) - Math.pow(high - data.L + data.R,2));
     prev.y2 = -prev.y1;
     console.log(prev.y1);
     for (var x = high + 0.01; x >= low; x = x - 0.001) {
-        var Yv1 = (rozrah.De/2) - Math.sqrt(Math.pow(rozrah.R,2) - Math.pow(x - rozrah.L + rozrah.R,2));
+        var Yv1 = (data.De/2) - Math.sqrt(Math.pow(data.R,2) - Math.pow(x - data.L + data.R,2));
         ctx.beginPath();
         ctx.moveTo(center.width - prev.x * koef, center.height - prev.y1 * koef);
         ctx.lineTo(center.width - x * koef, center.height - Yv1 * koef);
@@ -332,25 +282,25 @@ function Radius(){
 }
 
 function Yvi1() {
-    var high = rozrah.L - rozrah.R;
+    var high = data.L - data.R;
     var low = 0;
     var prev = {};
     prev.x = low;
 
-    prev.y1_2 = Math.sqrt((1 / Math.PI) * (Math.PI * Math.pow(Yop2(0), 2) + rozrah.F2 ));
-    prev.y2_2 = -Math.sqrt((1 / Math.PI) * (Math.PI * Math.pow(Yop2(0), 2) + rozrah.F2 ));
+    prev.y1_2 = Math.sqrt((1 / Math.PI) * (Math.PI * Math.pow(Yop2(0), 2) + data.F2 ));
+    prev.y2_2 = -Math.sqrt((1 / Math.PI) * (Math.PI * Math.pow(Yop2(0), 2) + data.F2 ));
 
-    prev.y1_1 = Math.sqrt((1 / Math.PI) * (Math.PI * Math.pow(Yop1(dano.Lukl-0.01), 2) + rozrah.F2 + ((rozrah.Fmin - rozrah.F2) / (rozrah.L - rozrah.R)) * (dano.Lukl-0.01) ));
-    prev.y2_1 = -Math.sqrt((1 / Math.PI) * (Math.PI * Math.pow(Yop1(dano.Lukl-0.01), 2) + rozrah.F2 + ((rozrah.Fmin - rozrah.F2) / (rozrah.L - rozrah.R)) * (dano.Lukl-0.01) ));
+    prev.y1_1 = Math.sqrt((1 / Math.PI) * (Math.PI * Math.pow(Yop1(data.Lukl-0.01), 2) + data.F2 + ((data.Fmin - data.F2) / (data.L - data.R)) * (data.Lukl-0.01) ));
+    prev.y2_1 = -Math.sqrt((1 / Math.PI) * (Math.PI * Math.pow(Yop1(data.Lukl-0.01), 2) + data.F2 + ((data.Fmin - data.F2) / (data.L - data.R)) * (data.Lukl-0.01) ));
     for (var x = low; x <= high; x = x + 0.01) {
-        if (x > dano.Lukl) {
-            var Yv1_1 = Math.sqrt((1 / Math.PI) * (Math.PI * Math.pow(Yop1(x), 2) + rozrah.F2 + ((rozrah.Fmin - rozrah.F2) / (rozrah.L - rozrah.R)) * x ));
+        if (x > data.Lukl) {
+            var Yv1_1 = Math.sqrt((1 / Math.PI) * (Math.PI * Math.pow(Yop1(x), 2) + data.F2 + ((data.Fmin - data.F2) / (data.L - data.R)) * x ));
             ctx.beginPath();
             ctx.moveTo(center.width - prev.x * koef, center.height - prev.y1_1 * koef);
             ctx.lineTo(center.width - x * koef, center.height - Yv1_1 * koef);
 
             ctx.stroke();
-            var Yv2_1 = -Math.sqrt((1 / Math.PI) * (Math.PI * Math.pow(Yop1(x), 2) + rozrah.F2 + ((rozrah.Fmin - rozrah.F2) / (rozrah.L - rozrah.R)) * x ));
+            var Yv2_1 = -Math.sqrt((1 / Math.PI) * (Math.PI * Math.pow(Yop1(x), 2) + data.F2 + ((data.Fmin - data.F2) / (data.L - data.R)) * x ));
             ctx.beginPath();
             ctx.moveTo(center.width - prev.x * koef, center.height - prev.y2_1 * koef);
             ctx.lineTo(center.width - x * koef, center.height - Yv2_1 * koef);
@@ -360,13 +310,13 @@ function Yvi1() {
             prev.y2_1 = Yv2_1;
         }
         else {
-            var Yv1_2 = Math.sqrt((1 / Math.PI) * (Math.PI * Math.pow(Yop2(x), 2) + rozrah.F2 + ((rozrah.Fmin - rozrah.F2) / (rozrah.L - rozrah.R)) * x )) ;
+            var Yv1_2 = Math.sqrt((1 / Math.PI) * (Math.PI * Math.pow(Yop2(x), 2) + data.F2 + ((data.Fmin - data.F2) / (data.L - data.R)) * x )) ;
             ctx.beginPath();
             ctx.moveTo(center.width - prev.x * koef, center.height - prev.y1_2 * koef);
             ctx.lineTo(center.width - x * koef, center.height - Yv1_2 * koef);
 
             ctx.stroke();
-            var Yv2_2 = -Math.sqrt((1 / Math.PI) * (Math.PI * Math.pow(Yop2(x), 2) + rozrah.F2 + ((rozrah.Fmin - rozrah.F2) / (rozrah.L - rozrah.R)) * x ));
+            var Yv2_2 = -Math.sqrt((1 / Math.PI) * (Math.PI * Math.pow(Yop2(x), 2) + data.F2 + ((data.Fmin - data.F2) / (data.L - data.R)) * x ));
             ctx.beginPath();
             ctx.moveTo(center.width - prev.x * koef, center.height - prev.y2_2 * koef);
             ctx.lineTo(center.width - x * koef, center.height - Yv2_2 * koef);
@@ -379,7 +329,11 @@ function Yvi1() {
     }
 }
 
-function setupCanvas(canvas) {
+document.getElementById('base').addEventListener("click",function() {
+    ServeBlocksFromDatabase();
+});
+
+/*function setupCanvas(canvas) {
     // Get the device pixel ratio, falling back to 1.
     var dpr = window.devicePixelRatio || 1;
     // Get the size of the canvas in CSS pixels.
@@ -393,4 +347,4 @@ function setupCanvas(canvas) {
     // don't have to worry about the difference.
     ctx.scale(dpr, dpr);
     return ctx;
-}
+}*/
